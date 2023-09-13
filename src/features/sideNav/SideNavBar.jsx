e0105642ef9ec9cba6282cf01e846188f4e945a3
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-09-13 14:44:12 +0300

    feat: Create & style SideNavBar component.

---
+++ src/features/sideNav/SideNavBar.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import "./SideNavBar.css";
+
+const SideNavBar = () => {
+	const [isExpanded, setExpendState] = useState(false);
+	const menuItems = [
+		{
+			text: "Home",
+			icon: "icons/grid.svg",
+		},
+		{
+			text: "Features",
+			icon: "icons/user.svg",
+		},
+		{
+			text: "Clients",
+			icon: "icons/message.svg",
+		},
+		{
+			text: "Settings",
+			icon: "icons/settings.svg",
+		},
+	];
+	return (
+		<div
+			className={
+				isExpanded
+					? "side-nav-container"
+					: "side-nav-container side-nav-container-NX"
+			}
+		>
+			<div className="nav-upper">
+				<div className="nav-heading">
+					{isExpanded && (
+						<div className="nav-brand">
+							<img src="icons/Logo.svg" alt="" srcSet="" />
+							<h2>Showkart</h2>
+						</div>
+					)}
+					<button
+						className={
+							isExpanded ? "hamburger hamburger-in" : "hamburger hamburger-out"
+						}
+						onClick={() => setExpendState(!isExpanded)}
+					>
+						<span></span>
+						<span></span>
+						<span></span>
+					</button>
+				</div>
+				<div className="nav-menu">
+					{menuItems.map(({ text, icon }) => (
+						<a
+							className={isExpanded ? "menu-item" : "menu-item menu-item-NX"}
+							href="#"
+							key={text}
+						>
+							<img className="menu-item-icon" src={icon} alt="" srcSet="" />
+							{isExpanded && <p>{text}</p>}
+						</a>
+					))}
+				</div>
+			</div>
+			<div className="nav-footer">
+				{isExpanded && (
+					<div className="nav-details">
+						<img
+							className="nav-footer-avatar"
+							src="icons/admin-avatar.svg"
+							alt=""
+							srcSet=""
+						/>
+						<div className="nav-footer-info">
+							<p className="nav-footer-user-name">M Showkat</p>
+							<p className="nav-footer-user-position">store admin</p>
+						</div>
+					</div>
+				)}
+				<img className="logout-icon" src="icons/logout.svg" alt="" srcSet="" />
+			</div>
+		</div>
+	);
+};
+
+export default SideNavBar;
